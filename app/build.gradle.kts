ba147046c0fa487acb63119c34e0bd0b15d0bca8
Hector <hector.mtzm6@gmail.com>
2026-08-26 19:51:15 -0500

    New Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         // the field. The wizard default of 35 would exclude almost all of them.
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.3.1"
+        versionCode = 6
+        versionName = "1.4.0"
 
         // Read from local.properties (gitignored, not versioned). Get a free key
         // at https://www.thetvdb.com/dashboard/account/apikeys
